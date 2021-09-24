fb01d5764cf2806d654a579b22f7eabd5f4c403f
Gaurav Jha <gkjha@live.com>
2021-09-24 13:00:30 +0530

    leet code solve

--- src/main/java/com/practice/application/RecursiveFile.java
+++ src/main/java/com/practice/application/RecursiveFile.java
@@ -1,15 +1,19 @@
 package com.practice.application;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-class Dir{
+class Dir {
     Dir dir;
+    String name;
     List<String> files;
 
-    public Dir(Dir dir, List<String> files) {
+    public Dir(Dir dir, List<String> files, String name) {
         this.dir = dir;
         this.files = files;
+        this.name = name;
     }
 
     public Dir getDir() {
@@ -30,42 +34,48 @@ class Dir{
 
     @Override
     public String toString() {
-        return "Dir{" +
-                "dir=" + dir +
-                ", files=" + files +
-                '}';
+        return "Name: " + name ;
+
     }
 }
+
 public class RecursiveFile {
     public static void main(String[] args) {
         List<Dir> directories = new ArrayList<>();
-       List<String> files= new ArrayList<>();
-       files.add("F7");
-       files.add("F8");
-       files.add("F9");
+        List<String> files = new ArrayList<>();
+        files.add("F7");
+        files.add("F8");
+        files.add("F9");
 
-        Dir d5 = new Dir(null,files);
-        files= new ArrayList<>();
+        Dir d5 = new Dir(null, files, "d5");
+        files = new ArrayList<>();
         files.add("F6");
         files.add("F5");
         files.add("F4");
 
-        Dir d4 = new Dir(d5,files);
+        Dir d4 = new Dir(d5, files, "d4");
 
-        files= new ArrayList<>();
+        files = new ArrayList<>();
         files.add("F3");
         files.add("F2");
         files.add("F1");
 
-        Dir d3 = new Dir(d4,files);
+        Dir d3 = new Dir(d4, files, "d3");
         directories.add(d3);
         directories.add(d4);
         directories.add(d5);
-        directories.stream().forEach(dir -> {
-            System.out.println(dir);
 
 
-        });
+        System.out.println(printDirSubfile(d3, new ArrayList<>()));
+    }
 
+    static Map<Dir, List<String>> printDirSubfile(Dir dir, List<String> files) {
+        if (dir == null) {
+            return new HashMap<>();
+        }
+        Map<Dir, List<String>> reuslt = printDirSubfile(dir.getDir(), files);
+        files.addAll(dir.getFiles());
+        reuslt.put(dir, new ArrayList<>(files));
+        return reuslt;
     }
 }
